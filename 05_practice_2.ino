21e2f33d885a408474ed431228264c9cf8434113
clockbar8072-cpu <clockbar8072@kookmin.ac.kr>
2026-09-15 10:29:56 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,1 +1,37 @@
 
+const int LED_PIN = 7;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+
+  for (int i = 0; i < 5; i++){
+  digitalWrite(LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+  delay(100);
+
+  }
+
+  digitalWrite(LED_PIN, LOW);
+
+  while (1) {
+
+  }
+
+}
+
+
+
+
+
+
